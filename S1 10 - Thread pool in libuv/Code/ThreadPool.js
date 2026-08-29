034d93887218901afeed78535e829a312b599354
Harshit Singh <45359928+harshit2490@users.noreply.github.com>
2026-08-29 14:38:52 +0530

    Add files via upload

---
+++ S1 10 - Thread pool in libuv/Code/ThreadPool.js
@@ -0,0 +1,30 @@
+const fs = require("fs");
+const crypto = require("crypto");
+
+process.env.UV_THREADPOOL_SIZE = 5;
+
+//The 4 keys will generate first and after that remaining one will be generated after some time because nodejs has only 4 threads at a time
+crypto.pbkdf2("Harshit", "salt", 500000, 10, "sha512", (err, key) => {
+  console.log("1st key generated below:");
+  console.log(key);
+});
+
+crypto.pbkdf2("Harshit", "salt", 500000, 10, "sha512", (err, key) => {
+  console.log("2 nd key generated below:");
+  console.log(key);
+});
+
+crypto.pbkdf2("Harshit", "salt", 500000, 10, "sha512", (err, key) => {
+  console.log("3 nd key generated below:");
+  console.log(key);
+});
+
+crypto.pbkdf2("Harshit", "salt", 500000, 10, "sha512", (err, key) => {
+  console.log("4 nd key generated below:");
+  console.log(key);
+});
+
+crypto.pbkdf2("Harshit", "salt", 500000, 10, "sha512", (err, key) => {
+  console.log("5 nd key generated below:");
+  console.log(key);
+});
